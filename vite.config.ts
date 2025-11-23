a7f614d65b3c224c84e2b27533a7defe82a169b3
Kaique Santos <kaique@agenciadev.com.br>
2025-11-23 15:27:38 -0300

    Remove define from vite.config to fix prototype error

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:3001'),
-  },
 }));
